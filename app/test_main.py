55980f488a71f03bf907b7d56cbafbf698dca2ab
Stas-dev <stasytkin0000@gmail.com>
2025-07-29 18:06:35 +0300

    +test

--- app/test_main.py
+++ app/test_main.py
@@ -8,3 +8,7 @@ client =  TestClient(app)
 def test_ner_aviability():
     response  = client.post("/ner/", json={"text":""})
     assert response.status_code  == 200
+
+def test_ner_bad_params():
+    response = client.post("/ner/", json={"":""})
+    assert response.status_code == 422
